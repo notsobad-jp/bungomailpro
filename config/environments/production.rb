f0df596b1bf228e7c19bfe9a8d7c7c190fb83e7a
tomomichi <tomomichi.onishi@gmail.com>
2018-10-13 16:54:38 +0900

    fix sender domain

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -71,7 +71,7 @@ Rails.application.configure do
   ActionMailer::Base.smtp_settings = {
     :user_name => ENV['SENDGRID_USERNAME'],
     :password => ENV['SENDGRID_PASSWORD'],
-    :domain => 'bungomail.herokuapp.com',
+    :domain => 'herokuapp.com',
     :address => 'smtp.sendgrid.net',
     :port => 587,
     :authentication => :plain,
