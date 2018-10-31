232120c1ad40bc06e6c2b7b8bab04adea673c2f4
tomomichi <tomomichi.onishi@gmail.com>
2018-10-31 15:16:07 +0900

    allow line break and linknize

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+  require "uri"
+
+  def linknize text
+    URI.extract(text, ['http', 'https']).uniq.each do |url|
+      sub_text = ""
+      sub_text << "<a href=" << url << " target=\"_blank\">" << url << "</a>"
+      text.gsub!(url, sub_text)
+    end
+    text
+  end
+
+  def simple_format_with_link text
+    simple_format(sanitize(linknize(text), attributes: ["href", "target"]), {}, sanitize: false)
+  end
 end
