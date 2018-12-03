6d12bb77f78a72fcab19fe28f28d6241bd678b24
tomomichi <tomomichi.onishi@gmail.com>
2018-12-03 12:27:02 +0900

    simplify routing

--- config/routes.rb
+++ config/routes.rb
@@ -77,5 +77,6 @@ Rails.application.routes.draw do
   get 'auth' => 'magic_tokens#auth', as: :auth
   post 'logout' => 'magic_tokens#destroy', as: :logout
 
-  root to: 'pages#top'
+  # root to: 'pages#top'
+  root to: 'subscriptions#index'
 end
