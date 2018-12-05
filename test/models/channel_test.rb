26095edfb1324cb8df271876c4afc201a989c982
tomomichi <tomomichi.onishi@gmail.com>
2018-12-05 14:23:22 +0900

    wip: add test

---
+++ test/models/channel_test.rb
@@ -0,0 +1,21 @@
+require 'test_helper'
+
+class ChannelTest < ActiveSupport::TestCase
+  test "the truth" do
+    assert true
+  end
+
+
+  test "current_chapter before delivery" do
+  end
+
+  test "current_chapter after delivery" do
+  end
+
+  test "set_next_chapter when next_chapter" do
+  end
+  test "set_next_chapter when !next_chapter && next_book" do
+  end
+  test "set_next_chapter when !next_chapter && !next_book" do
+  end
+end
