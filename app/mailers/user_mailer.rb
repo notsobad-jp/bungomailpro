b8d44d56e7d70d4b80c0ee9ac7eb014b7cc8a0ee
tomomichi <tomomichi.onishi@gmail.com>
2018-12-11 13:37:03 +0900

    :recycle: Fix warning message, account info on sidebar

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -32,7 +32,7 @@ class UserMailer < ApplicationMailer
     mail(
       from: "#{@chapter.book.author}（ブンゴウメール） <bungomail@notsobad.jp>",
       to: 'bungomail@notsobad.jp',
-      subject: "【#{@channel.title}】#{@chapter.book.title}（#{@chapter.index}/#{@chapter.book.chapters_count}）"
+      subject: "#{@chapter.book.title}（#{@chapter.index}/#{@chapter.book.chapters_count}）【#{@channel.title}】"
     )
     Logger.new(STDOUT).info "[SCHEDULED] channel:#{@channel.id}, chapter:#{@chapter.id}, send_at:#{send_at}, to:#{@channel.subscribers.pluck(:id)}"
   end
