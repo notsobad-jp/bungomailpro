63c56b6fb257895c4a47ae5382ed6d3c71312a53
tomomichi <tomomichi.onishi@gmail.com>
2019-01-25 17:32:10 +0900

    add test for subscription

--- test/models/subscription_test.rb
+++ test/models/subscription_test.rb
@@ -24,7 +24,7 @@ class SubscriptionTest < ActiveSupport::TestCase
   ########################################################################
   # current_chapter
   ########################################################################
-  ## 配信開始前
+  # 配信開始前
   test "current_chapter_when_not_started" do
     sub = subscriptions(:user1_channel1)
 
@@ -39,25 +39,25 @@ class SubscriptionTest < ActiveSupport::TestCase
     end
   end
 
-  ## 配信完了後
+  # 配信完了後
   test "current_chapter_when_finished" do
     sub = subscriptions(:user2_channel1)
     assert_nil sub.current_chapter
   end
 
-  ## 配信中（配信時間前）
+  # 配信中（配信時間前）
   test "current_chapter_before_delivery_time" do
     travel_to(Time.zone.now.change(hour: 6)) do
       sub = subscriptions(:user1_channel2)
-      assert_equal chapters(:book2_chapter1), sub.current_chapter
+      assert_equal chapters(:book2_chapter2), sub.current_chapter
     end
   end
 
-  ## 配信中（配信時間後）
+  # 配信中（配信時間後）
   test "current_chapter_after_delivery_time" do
     travel_to(Time.zone.now.change(hour: 10)) do
       sub = subscriptions(:user1_channel2)
-      assert_equal chapters(:book2_chapter2), sub.current_chapter
+      assert_equal chapters(:book2_chapter3), sub.current_chapter
     end
   end
 
@@ -65,13 +65,117 @@ class SubscriptionTest < ActiveSupport::TestCase
   ########################################################################
   # prev_chapter
   ########################################################################
-  test "prev_chapter" do
-    # index > 1のとき
-    # 2冊めのindex:1
-    # 1冊目のindex:1
-    # 配信完了後
+  # index > 1のとき
+  test "prev_chapter_with_index>1" do
+    sub = subscriptions(:user1_channel2)
+    assert_equal chapters(:book2_chapter2), sub.prev_chapter
   end
 
-  test "set_next_chapter" do
+  # 2冊めのindex:1
+  test "prev_chapter_with_2nd_book_index_1" do
+    sub = subscriptions(:user2_channel2)
+    assert_equal chapters(:book2_chapter3), sub.prev_chapter
+  end
+
+  # 1冊目のindex:1
+  test "prev_chapter_with_1st_book_index_1" do
+    sub = subscriptions(:user1_channel1)
+    assert_nil sub.prev_chapter
+  end
+
+  # 配信完了後
+  test "prev_chapter_with_finished_channel" do
+    sub = subscriptions(:user2_channel1)
+    assert_nil sub.prev_chapter
+  end
+
+
+  ########################################################################
+  # set_next_chapter
+  ########################################################################
+  # 同じ本で次のchapterが存在すればそれをセット
+  test "set_next_chapter_when_next_chapter_exists" do
+    travel_to(Time.zone.now.change(day: 1)) do
+      sub = subscriptions(:user1_channel1)
+      sub.set_next_chapter
+
+      assert_equal 2, sub.next_chapter_index
+      assert_equal Time.zone.today.change(day: 2), sub.next_delivery_date
+      assert_equal books(:book1), sub.current_book
+    end
+  end
+
+  # 次のchapterがなければ、次の本を探してindex:1でセット
+  test "set_next_chapter_when_next_book_exists" do
+    travel_to(Time.zone.now.change(day: 1)) do
+      sub = subscriptions(:user1_channel2)
+      sub.set_next_chapter
+
+      assert_equal 1, sub.next_chapter_index
+      assert_equal Time.zone.today.change(day: 2), sub.next_delivery_date
+      assert_equal books(:book1), sub.current_book
+    end
+  end
+
+  # next_channel_bookもなければ配信停止状態にする
+  test "set_next_chapter_when_no_next_exists" do
+    travel_to(Time.zone.now.change(day: 1)) do
+      sub = subscriptions(:user3_channel1)
+      sub.set_next_chapter
+
+      assert_nil sub.next_chapter_index
+      assert_nil sub.next_delivery_date
+      assert_nil sub.current_book
+    end
+  end
+
+  # すでに配信停止の状態なら処理をスキップ
+  test "set_next_chapter_when_already_finished" do
+    travel_to(Time.zone.now.change(day: 1)) do
+      sub = subscriptions(:user2_channel1)
+      sub.set_next_chapter
+
+      assert_nil sub.next_chapter_index
+      assert_nil sub.next_delivery_date
+      assert_nil sub.current_book
+    end
+  end
+
+  # 翌日が31日の場合はスキップ
+  test "set_next_chapter_when_tomorrow_31st" do
+    travel_to('2019-01-30') do
+      sub = subscriptions(:user1_channel1)
+      sub.set_next_chapter
+
+      assert_equal 2, sub.next_chapter_index
+      assert_equal Date.parse('2019-02-01'), sub.next_delivery_date
+      assert_equal books(:book1), sub.current_book
+    end
+  end
+
+
+  ########################################################################
+  # not_started
+  ########################################################################
+  # 配信前
+  test "not_started_when_true" do
+    sub = subscriptions(:user1_channel1)
+    assert sub.not_started?
+  end
+
+  # 配信後
+  test "not_started_when_false" do
+    sub = subscriptions(:user1_channel2)
+    assert !sub.not_started?
+  end
+
+
+  ########################################################################
+  # create_feed
+  ########################################################################
+  test "create_feed" do
+    sub = subscriptions(:user1_channel1)
+    sub.create_feed
+    assert_equal 1, sub.feeds.count
   end
 end
