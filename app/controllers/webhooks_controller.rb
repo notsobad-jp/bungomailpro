49ff91e3f4b6e7e6b07cefe6d3b2ff6f4d9e0b90
tomomichi <tomomichi.onishi@gmail.com>
2019-02-02 11:13:49 +0900

    check signature for webhook

--- app/controllers/webhooks_controller.rb
+++ app/controllers/webhooks_controller.rb
@@ -1,22 +1,29 @@
 class WebhooksController < ApplicationController
   protect_from_forgery with: :null_session
 
-  # Stripe側でsubscriptionのステータスが変更されたのを拾ってDBに反映する
+  # Stripe側でsubscriptionのステータスが変更されたのを拾ってDBに反映する（status以外の情報は更新しない）
   ## 1) トライアル終了（trialing→active）
   ## 2) 支払い失敗（active→past_due）
-  ## 3) 自動解約（past_due→canceled）
+  ## 3) 支払い失敗→再支払（past_due→active
+  ## 4) 支払い失敗→自動解約（past_due→canceled）
   def update_subscription
-    event_json = JSON.parse(request.body.read)
+    payload = request.body.read
+    sig_header = request.env['HTTP_STRIPE_SIGNATURE']
 
     # statusが変更されてない場合はスキップ
-    return head 200 if !event_json['data']['previous_attributes'].has_key?('status')
+    return head 200 if !JSON.parse(payload)['data']['previous_attributes'].try(:has_key?, 'status')
 
-    # 不正対策のためにEventオブジェクトをStripeから取り直す
+    # webhookのsignatureチェック
     begin
-      event = Stripe::Event.retrieve(event_json['id'])
-    rescue Stripe::InvalidRequestError => e
-      logger.error "[STRIPE] Event not found: #{e}"
-      return head 500
+      event = Stripe::Webhook.construct_event( payload, sig_header, ENV['STRIPE_WEBHOOK_SIGNATURE'] )
+    rescue JSON::ParserError => e
+      # Invalid payload
+      status 400
+      return
+    rescue Stripe::SignatureVerificationError => e
+      # Invalid signature
+      status 400
+      return
     end
 
     # DBに変更を反映
