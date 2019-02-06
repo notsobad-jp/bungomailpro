922cf108d216d73bc77212389490728bdd8619c3
tomomichi <tomomichi.onishi@gmail.com>
2019-02-06 11:37:28 +0900

    add test for subscriptions_controller

---
+++ test/controllers/subscriptions_controller_test.rb
@@ -0,0 +1,32 @@
+require 'test_helper'
+class SubscriptionsControllerTest < ActionDispatch::IntegrationTest
+  def setup
+    # Gravatarへのリクエストmock
+    stub_request(:get, /https:\/\/ja\.gravatar\.com\/.*\.json/).to_return(status: 200, body: File.read("test/fixtures/files/gravatar.json"))
+  end
+
+
+  ########################################################################
+  # edit
+  ########################################################################
+  test "access_edit_when_guest" do
+    sub = subscriptions(:user1_channel1)
+    get edit_subscription_path(sub.token)
+    assert_redirected_to login_path
+  end
+
+  test "access_edit_when_owner" do
+    sub = subscriptions(:user1_channel1)
+    login_user(sub.user)
+    get edit_subscription_path(sub.token)
+    assert_response :success
+  end
+
+  test "access_edit_when_other" do
+    sub = subscriptions(:user1_channel1)
+    user = users(:user2)
+    login_user(user)
+    get edit_subscription_path(sub.token)
+    assert_redirected_to pro_root_path
+  end
+end
