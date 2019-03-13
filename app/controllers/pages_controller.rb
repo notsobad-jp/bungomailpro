dfb81647f8582afede609f36bfc455a7a0179585
tomomichi <tomomichi.onishi@gmail.com>
2019-03-13 15:57:51 +0900

    :pencil2: Update documents

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -12,8 +12,28 @@ class PagesController < ApplicationController
     @popular_books = ChannelBook.includes(:book).order(created_at: :desc).map(&:book).uniq.take(12)
   end
 
+  def index
+    @pages = page_titles
+    @breadcrumbs << { name: 'ドキュメント' }
+  end
+
   def show
-    page_titles = {
+    @page_title = page_titles[params[:page].to_sym]
+    raise ActionController::RoutingError, request.url unless @page_title
+
+    @meta_title = @page_title
+    @meta_description = "#{@page_title}のページです。"
+    @meta_keywords = @page_title
+    @breadcrumbs << { name: 'ドキュメント', url: pages_path }
+    @breadcrumbs << { name: @page_title }
+
+    render params[:page]
+  end
+
+  private
+
+  def page_titles
+    {
       about: 'ブンゴウメールとは？',
       plan: '料金プラン',
       faq: 'よくある質問',
@@ -21,14 +41,5 @@ class PagesController < ApplicationController
       privacy: 'プライバシーポリシー',
       tokushoho: '特定商取引法に基づく表示',
     }
-    @page_title = page_titles[params[:page].to_sym]
-    raise ActionController::RoutingError, request.url unless @page_title
-
-    @meta_title = @page_title
-    @meta_description = "#{@page_title}のページです。"
-    @meta_keywords = @page_title
-    @breadcrumbs << { name: @page_title }
-
-    render params[:page]
   end
 end
