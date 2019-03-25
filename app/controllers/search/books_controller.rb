0c49075898a37c8014faf0fb60f38eec48a92703
tomomichi <tomomichi.onishi@gmail.com>
2019-03-25 13:43:50 +0900

    bungo sidebar

--- app/controllers/search/books_controller.rb
+++ app/controllers/search/books_controller.rb
@@ -4,6 +4,7 @@ class Search::BooksController < ApplicationController
 
   def search
     category_id = Category.find_by(id: params[:category_id]).try(:id) || 'all'
+
     author_id = Book.find_by(author: params[:author_name]).try(:author_id) || 'all'
 
     redirect_to books_search_author_path(id: author_id, category_id: category_id)
@@ -15,7 +16,7 @@ class Search::BooksController < ApplicationController
 
     book = Book.find_by(author_id: params[:id])
     @author = { id: book.try(:author_id) || 'all', name: book.try(:author) || 'すべての著者' }
-    @authors = Book.group(:author).pluck(:author)
+    @authors = Book.limit(100).order('sum_access_count desc').group(:author, :author_id).sum(:access_count)
 
     query = Book.where(words_count: (@category.range_from..@category.range_to))
     query = query.where(author_id: @author[:id]) if @author[:id] != 'all'
