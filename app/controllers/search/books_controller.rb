ad2b54011982880cb08caf0edd8a7112db7645dc
o_tomomichi <tomomichi.onishi@gmail.com>
2019-04-25 16:13:14 +0900

    :+1: SEO update (#62)
    * Unique h1 title, remove microdata
    
    * Fix breadcrumbs
    
    * update sitemap

--- app/controllers/search/books_controller.rb
+++ app/controllers/search/books_controller.rb
@@ -9,7 +9,7 @@ class Search::BooksController < Search::ApplicationController
     @meta_keywords = "#{@author[:name]},#{@category.name}で読める,#{@category.title}"
     @meta_canonical_url = root_path if @author[:id] == 'all' && @category.id == 'all'
 
-    @breadcrumbs << { name: @author[:name], url: author_category_books_path(author_id: @author[:id], category_id: 'all')} unless @author[:id] == 'all'
+    @breadcrumbs << { name: @author[:name], url: author_category_books_url(author_id: @author[:id], category_id: 'all')} unless @author[:id] == 'all'
     category_name = @category.id == 'all' ? '全作品' : "#{@category.title}（#{@category.name}）"
     @breadcrumbs << { name: category_name }
   end
@@ -24,9 +24,9 @@ class Search::BooksController < Search::ApplicationController
     @meta_description = "『#{@book.title}』は#{@author[:name]}の#{@category.title}作品。#{@book.words_count.to_s(:delimited)}文字で、おおよそ#{@category.name}で読むことができます。"
     @meta_keywords = [@book.title, @author[:name], @category.title, @category.name].join(",")
 
-    @breadcrumbs << { name: @author[:name], url: author_category_books_path(author_id: @author[:id], category_id: 'all')}
+    @breadcrumbs << { name: @author[:name], url: author_category_books_url(author_id: @author[:id], category_id: 'all')}
     category_name = "#{@category.title}（#{@category.name}）"
-    @breadcrumbs << { name: category_name, url: author_category_books_path(author_id: @author[:id], category_id: @category.id) }
+    @breadcrumbs << { name: category_name, url: author_category_books_url(author_id: @author[:id], category_id: @category.id) }
     @breadcrumbs << { name: @book.title }
   end
 
@@ -34,18 +34,20 @@ class Search::BooksController < Search::ApplicationController
 
   def search_page_title
     author_name = @author[:id] == 'all' ? '青空文庫' : @author[:name]
-    if @category.id == 'all'
-      "#{author_name}の全作品"
-    else
-      "#{@category.name}で読める#{author_name}の#{@category.title}作品"
-    end
+    title = if @category.id == 'all'
+              "#{author_name}の全作品"
+            else
+              "#{@category.name}で読める#{author_name}の#{@category.title}作品"
+            end
+    title += "（#{params[:page]}ページ目）" if params[:page] && params[:page].to_i > 1
+    title
   end
 
   def search_page_description
     if @category.id == 'all'
-      "青空文庫で公開されている#{@author[:name]}の全作品一覧です。"
+      "青空文庫で公開されている#{@author[:name]}の全作品を、おすすめ人気順で表示しています。"
     else
-      "青空文庫で公開されている#{@author[:name]}の作品の中で、おおよその読了目安時間が「#{@category.name}」の#{@category.title}#{@books.total_count.to_s(:delimited)}作品です。"
+      "青空文庫で公開されている#{@author[:name]}の作品の中で、おおよその読了目安時間が「#{@category.name}」の#{@category.title}#{@books.total_count.to_s(:delimited)}作品を、おすすめ人気順に表示しています。"
     end
   end
 end
