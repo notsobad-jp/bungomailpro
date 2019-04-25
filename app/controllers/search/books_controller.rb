64119a9235972917dbd1d6bff267ace234f299e6
tomomichi <tomomichi.onishi@gmail.com>
2019-04-25 16:07:00 +0900

    Fix breadcrumbs

--- app/controllers/search/books_controller.rb
+++ app/controllers/search/books_controller.rb
@@ -9,7 +9,7 @@ class Search::BooksController < Search::ApplicationController
     @meta_keywords = "#{@author[:name]},#{@category.name}で読める,#{@category.title}"
     @meta_canonical_url = root_path if @author[:id] == 'all' && @category.id == 'all'
 
-    @breadcrumbs << { name: @author[:name], url: author_category_books_path(author_id: @author[:id], category_id: 'all')} unless @author[:id] == 'all'
+    @breadcrumbs << { name: @author[:name], url: author_category_books_url(author_id: @author[:id], category_id: 'all')} unless @author[:id] == 'all'
     category_name = @category.id == 'all' ? '全作品' : "#{@category.title}（#{@category.name}）"
     @breadcrumbs << { name: category_name }
   end
@@ -24,9 +24,9 @@ class Search::BooksController < Search::ApplicationController
     @meta_description = "『#{@book.title}』は#{@author[:name]}の#{@category.title}作品。#{@book.words_count.to_s(:delimited)}文字で、おおよそ#{@category.name}で読むことができます。"
     @meta_keywords = [@book.title, @author[:name], @category.title, @category.name].join(",")
 
-    @breadcrumbs << { name: @author[:name], url: author_category_books_path(author_id: @author[:id], category_id: 'all')}
+    @breadcrumbs << { name: @author[:name], url: author_category_books_url(author_id: @author[:id], category_id: 'all')}
     category_name = "#{@category.title}（#{@category.name}）"
-    @breadcrumbs << { name: category_name, url: author_category_books_path(author_id: @author[:id], category_id: @category.id) }
+    @breadcrumbs << { name: category_name, url: author_category_books_url(author_id: @author[:id], category_id: @category.id) }
     @breadcrumbs << { name: @book.title }
   end
 
