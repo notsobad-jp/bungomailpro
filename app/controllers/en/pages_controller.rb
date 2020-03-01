69319956d5f1682bcca5ce0cb137345ef7592fdf
o_tomomichi <tomomichi.onishi@gmail.com>
2020-03-02 00:56:19 +0900

    :+1: Fix new user login and existing user signup (#83)
    * :+1: Send login email to existing user, add required to email field
    
    * :+1: Fix non-existing user login

--- app/controllers/en/pages_controller.rb
+++ app/controllers/en/pages_controller.rb
@@ -2,7 +2,6 @@ class En::PagesController < En::ApplicationController
   skip_before_action :require_login
 
   def lp
-    flash[:success] = "Your account has been successfully saved! The email will be delivered from tomorrow and we hope you like it :)" if params[:success]
     render layout: false
   end
 end
