358bc73d6111dade7619de7d11a604712041ba93
tomomichi <tomomichi.onishi@gmail.com>
2020-03-15 14:52:04 +0900

    :green_heart: Add test for access_rating

---
+++ test/models/book_test.rb
@@ -0,0 +1,27 @@
+require 'test_helper'
+
+class BookTest < ActiveSupport::TestCase
+  test "access_rating: 1000" do
+    book = Book.new(access_count: 10000)
+    assert_equal 5, book.access_rating
+    assert_equal 3, book.access_rating(:stars)
+  end
+
+  test "access_rating: 500" do
+    book = Book.new(access_count: 500)
+    assert_equal 4.5, book.access_rating
+    assert_equal 2, book.access_rating(:stars)
+  end
+
+  test "access_rating: 1" do
+    book = Book.new(access_count: 1)
+    assert_equal 4, book.access_rating
+    assert_equal 1, book.access_rating(:stars)
+  end
+
+  test "access_rating: 0" do
+    book = Book.new(access_count: 0)
+    assert_equal 3, book.access_rating
+    assert_equal 0, book.access_rating(:stars)
+  end
+end
