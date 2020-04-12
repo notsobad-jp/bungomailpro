c84bae3129c7b91ca5efb31567bcec7a1c27c7b9
tomomichi <tomomichi.onishi@gmail.com>
2020-04-12 15:31:33 +0900

    :sparkles: Enable book stocking

--- app/controllers/en/application_controller.rb
+++ app/controllers/en/application_controller.rb
@@ -1,4 +1,10 @@
 class En::ApplicationController < ApplicationController
   layout 'en/layouts/application'
-  before_action :require_login
+  before_action :require_login, :switch_locale
+
+  def switch_locale
+    I18n.locale = :en
+    book_class = GutenBook
+    Object.const_set :Book, Class.new(book_class)
+  end
 end
