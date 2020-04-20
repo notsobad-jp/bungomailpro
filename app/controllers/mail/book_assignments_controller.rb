ceb344f041dcd8801fc8fd2bb225133bd575a2ed
tomomichi <tomomichi.onishi@gmail.com>
2020-04-20 21:53:22 +0900

    :+1: Fix book_assignments addition from search

--- app/controllers/mail/book_assignments_controller.rb
+++ app/controllers/mail/book_assignments_controller.rb
@@ -1,9 +1,9 @@
 class Mail::BookAssignmentsController < Mail::ApplicationController
-  before_action :set_channel_and_book, only: [:new, :create]
+  before_action :set_book, only: [:new, :create]
 
   def new
-    @book_assignment = @channel.book_assignments.new
-    @book_assignment.book = @book
+    @channel = current_user.default_channel
+    @book_assignment = @channel.book_assignments.new(book_id: @book.id, book_type: @book.class.name)
 
     @categories = @book.class::CATEGORIES
     @category = @categories[@book.category_id.to_sym]
@@ -13,15 +13,16 @@ class Mail::BookAssignmentsController < Mail::ApplicationController
   end
 
   def create
-    @book_assignment = @channel.book_assignments.new
-    @book_assignment.book = @book
+    @channel = Channel.find(params[:channel_id])
+    @book_assignment = @channel.book_assignments.new(book_id: @book.id, book_type: @book.class.name)
 
     if @book_assignment.save
       flash[:success] = 'Added the stocked book successfully!'
+      redirect_to channel_path(@channel)
     else
       flash[:error] = 'Sorry somethin went wrong. Please check the data and try again.'
+      redirect_to mypage_path
     end
-    redirect_to mypage_path
   end
 
   # 現在のBookAssignmentを削除して新しい本をセット
@@ -32,22 +33,21 @@ class Mail::BookAssignmentsController < Mail::ApplicationController
     @book_assignment.channel.delay.assign_book_and_set_feeds
 
     flash[:success] = 'Skipped successfully! The new book will be sent from tomorrow.'
-    redirect_to mypage_path
+    redirect_to channel_path(@book_assignment.channel)
   end
 
   def destroy
     @book_assignment = BookAssignment.find(params[:id])
     @book_assignment.destroy
     flash[:success] = 'Deleted the stocked book successfully!'
-    redirect_to mypage_path
+    redirect_to channel_path(@book_assignment.channel)
   end
 
   private
 
-  def set_channel_and_book
-    @channel = Channel.find(params[:channel_id])
+  def set_book
     book_class = params[:book_type].constantize if %w(AozoraBook GutenBook).include?(params[:book_type])
     @book = book_class&.find_by(id: params[:book_id])
-    raise ActiveRecord::RecordNotFound unless @channel && @book&.category && @book&.author_id
+    raise ActiveRecord::RecordNotFound unless @book&.category && @book&.author_id
   end
 end
