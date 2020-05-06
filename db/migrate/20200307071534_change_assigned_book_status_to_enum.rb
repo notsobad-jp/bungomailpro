273e724a6d73cc014982dccd683a7e9927f4d84e
notsobad <tomomichi.onishi@gmail.com>
2020-05-06 13:15:24 +0900

    :bug: Fix broken migration

--- db/migrate/20200307071534_change_assigned_book_status_to_enum.rb
+++ db/migrate/20200307071534_change_assigned_book_status_to_enum.rb
@@ -1,10 +1,10 @@
 class ChangeAssignedBookStatusToEnum < ActiveRecord::Migration[5.2]
   def up
     execute 'ALTER TABLE assigned_books ALTER COLUMN status DROP DEFAULT;'
-    change_column :assigned_books, :status, 'integer USING CAST(status AS integer)', default: 0, null: false
+    # change_column :assigned_books, :status, 'integer USING CAST(status AS integer)', default: 0, null: false
   end
 
   def down
-    change_column :assigned_books, :status, :string
+    # change_column :assigned_books, :status, :string
   end
 end
