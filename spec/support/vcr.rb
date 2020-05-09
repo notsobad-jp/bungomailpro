043905be53e3af9071d1e7c1f22db302d1090e03
notsobad <tomomichi.onishi@gmail.com>
2020-05-09 17:35:09 +0900

    :bug: use vcr sensitive_filter for sendgrid api_key

--- spec/support/vcr.rb
+++ spec/support/vcr.rb
@@ -1,8 +1,8 @@
 VCR.configure do |c|
     c.cassette_library_dir = 'spec/vcr'
-    # c.configure_rspec_metadata!
     c.hook_into :webmock
     c.ignore_localhost = true
+    c.filter_sensitive_data('<SENDGRID_API_KEY>') { ENV['SENDGRID_API_KEY'] }
     c.default_cassette_options = {
       record: :once,
       match_requests_on: [:method, :uri, :body],
