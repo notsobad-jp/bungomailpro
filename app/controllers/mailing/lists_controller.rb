bdd8913db644dfd9569bd7567450d873e7a54626
notsobad <tomomichi.onishi@gmail.com>
2020-05-30 19:38:34 +0900

    :+1: Hide future campaign

--- app/controllers/mailing/lists_controller.rb
+++ app/controllers/mailing/lists_controller.rb
@@ -4,7 +4,7 @@ class Mailing::ListsController < Mailing::ApplicationController
   def books
     year = params[:year] || Time.current.year
     start = Time.current.change(year: year).beginning_of_year
-    @campaign_groups = CampaignGroup.includes(:book).where(list_id: params[:id], start_at: start..start.end_of_year).order(:start_at)
+    @campaign_groups = CampaignGroup.includes(:book).where(list_id: params[:id], start_at: start..start.end_of_year).where("start_at < ?", Time.current).order(:start_at)
 
     @meta_title = "ブンゴウメールの過去配信作品一覧 - #{year}年"
     @meta_description = 'これまでにブンゴウメールで配信した作品の一覧です。'
