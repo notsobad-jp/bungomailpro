4f6562a4cc417f359c861f9e9721d7255a856a81
notsobad <tomomichi.onishi@gmail.com>
2021-02-03 16:31:46 +0900

    restore lists page

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,7 @@
+class ListsController < ApplicationController
+  def books
+    year = params[:year] || Time.current.year
+    start = Time.current.change(year: year).beginning_of_year
+    @campaign_groups = CampaignGroup.includes(:book).where(list_id: params[:id], start_at: start..start.end_of_year).where("start_at < ?", Time.current).order(:start_at)
+  end
+end
