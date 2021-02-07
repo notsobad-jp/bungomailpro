3742971005f54c43905b16ddc3c4e8a6d54d9529
notsobad <tomomichi.onishi@gmail.com>
2021-02-07 12:09:32 +0900

    fix meta and static pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,20 @@
 class PagesController < ApplicationController
-  def show
-    @page_title = page_titles[params[:page].to_sym]
-    raise ActionController::RoutingError, request.url unless @page_title
+  def lp
+    @meta_title = 'ブンゴウメール | 1日3分のメールでムリせず毎月1冊本が読める、忙しいあなたのための読書サポートサービス'
+    @meta_description = '青空文庫の作品を小分けにして、毎朝メールで配信。気づいたら毎月1冊本が読めてしまう、忙しいあなたのための読書サポートサービスです。'
+  end
 
+  def show
+    @meta_title = page_titles[params[:page].to_sym]
+    raise ActionController::RoutingError, request.url unless @meta_title
+    @meta_description = @meta_title
     render params[:page]
+  end
+
+  def dogramagra
+    @meta_title = "ドグラ・マグラ365日配信チャレンジ"
+    @meta_description = "夢野久作『ドグラ・マグラ』を、365日かけて毎日メールで少しずつ配信します。"
+    @meta_image = "https://bungomail.com/assets/images/campaigns/dogramagra.png"
   end
 
   private
