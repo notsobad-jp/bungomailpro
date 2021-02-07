761635220b3e17a62c25de8f56d51dea97576d9f
notsobad <tomomichi.onishi@gmail.com>
2021-02-07 13:11:20 +0900

    fix list page

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -3,5 +3,6 @@ class ListsController < ApplicationController
     year = params[:year] || Time.current.year
     start = Time.current.change(year: year).beginning_of_year
     @campaign_groups = CampaignGroup.includes(:book).where(list_id: params[:id], start_at: start..start.end_of_year).where("start_at < ?", Time.current).order(:start_at)
+    @meta_title = "過去配信作品（#{year}）"
   end
 end
