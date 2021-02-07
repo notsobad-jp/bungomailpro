13e10e75d0471d21825230a6ee0f24d0128fab0a
notsobad <tomomichi.onishi@gmail.com>
2021-02-07 15:08:59 +0900

    udpate campaign tasks with bitly api

--- app/models/campaign_group.rb
+++ app/models/campaign_group.rb
@@ -60,6 +60,11 @@ class CampaignGroup < ApplicationRecord
     end
   end
 
+  def twitter_short_url
+    res = Bitly.call(path: 'shorten', params: { long_url: self.twitter_long_url })
+    res["link"]
+  end
+
   def twitter_long_url
     # CGI.escape("https://twitter.com/intent/tweet?url=https%3A%2F%2Fbungomail.com%2F&hashtags=ブンゴウメール&text=#{campaigns.first.send_at.in_time_zone("Tokyo").month}月は%20%23#{book.author.delete(' ')}%20%23#{book.title}%20を配信中！")
     "https://twitter.com/intent/tweet?url=https%3A%2F%2Fbungomail.com%2F&hashtags=ブンゴウメール,青空文庫&text=#{start_at.month}月は%20%23#{book.author.delete(' ')}%20%23#{book.title}%20を配信中！"
