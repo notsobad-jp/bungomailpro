134c0348a1201a23fd046ef5646216ecb6729af2
notsobad <tomomichi.onishi@gmail.com>
2021-02-09 11:53:43 +0900

    fix sendgrid settings to use apikey

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,8 +61,8 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   # config.action_mailer.raise_delivery_errors = false
   ActionMailer::Base.smtp_settings = {
-    :user_name => ENV['SENDGRID_USERNAME'],
-    :password => ENV['SENDGRID_PASSWORD'],
+    :user_name => 'apikey',
+    :password => ENV['SENDGRID_API_KEY'],
     :domain => 'bungomail.com',
     :address => 'smtp.sendgrid.net',
     :port => 587,
