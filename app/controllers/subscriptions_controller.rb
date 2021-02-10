d0366142795c9a05e58af5415d417cadbe44c703
notsobad <tomomichi.onishi@gmail.com>
2021-02-10 16:02:12 +0900

    wip: fix google auth

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -7,18 +7,19 @@ class SubscriptionsController < ApplicationController
     email = params[:email]
     p email
 
-    scope = ['https://www.googleapis.com/auth/admin.directory.group']
+    scope = [
+      'https://www.googleapis.com/auth/admin.directory.group',
+      # 'https://www.googleapis.com/auth/admin.directory.member',
+    ]
     service = Google::Apis::AdminDirectoryV1::DirectoryService.new
-    # authorization = Google::Auth::ServiceAccountCredentials.make_creds(
-    #   json_key_io: File.open('tmp/client_secrets.json'),
-    #   scope: scope
-    # )
-    ENV['GOOGLE_APPLICATION_CREDENTIALS']  = "tmp/client_secrets.json"
-    authorization = Google::Auth.get_application_default(scope).dup
-
+    authorization = Google::Auth::ServiceAccountCredentials.make_creds(
+      json_key_io: StringIO.new(ENV['GOOGLE_SERVICE_ACCOUNT_KEY']),
+      scope: scope
+    )
     authorization.sub = 'info@notsobad.jp'
     service.authorization = authorization
 
+    p service.get_member('test@notsobad.jp', 'info@notsobad.jp')
     p service.list_groups(customer: 'my_customer')
 
     redirect_to root_path
