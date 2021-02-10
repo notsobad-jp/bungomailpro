a29e8403943bee29b72aa9430fd04475dc5deeb8
notsobad <tomomichi.onishi@gmail.com>
2021-02-10 16:43:47 +0900

    liblize google-directory-service

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -1,24 +1,10 @@
-require 'google/apis/admin_directory_v1'
-require 'googleauth'
-
 class SubscriptionsController < ApplicationController
   def create
     p "create!"
     email = params[:email]
     p email
 
-    scope = [
-      'https://www.googleapis.com/auth/admin.directory.group',
-      # 'https://www.googleapis.com/auth/admin.directory.member',
-    ]
-    service = Google::Apis::AdminDirectoryV1::DirectoryService.new
-    authorization = Google::Auth::ServiceAccountCredentials.make_creds(
-      json_key_io: StringIO.new(ENV['GOOGLE_SERVICE_ACCOUNT_KEY']),
-      scope: scope
-    )
-    authorization.sub = 'info@notsobad.jp'
-    service.authorization = authorization
-
+    service = GoogleDirectoryService.instance
     p service.get_member('test@notsobad.jp', 'info@notsobad.jp')
     p service.list_groups(customer: 'my_customer')
 
