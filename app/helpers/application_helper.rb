10bd09ff952780747c35c6db6d300e73cb97bdac
notsobad <tomomichi.onishi@gmail.com>
2021-02-11 17:23:09 +0900

    remove unused helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,2 @@
 module ApplicationHelper
-  # テキスト中のリンクをaタグ化、改行をbrタグ化する
-  def htmlify(text)
-    URI.extract(text, ["http", "https"]).uniq.each do |url|
-      text.gsub!(url, "<a target='_blank' href='#{url}'>#{url}</a>")
-    end
-    text.gsub!(/(\r\n|\r|\n)/, "<br />")
-    text
-  end
 end
