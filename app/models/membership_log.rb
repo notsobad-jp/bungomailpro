793e927850d1ca79690e776e35b3579511989398
notsobad <tomomichi.onishi@gmail.com>
2021-02-17 16:38:06 +0900

    add apply_change method to membership_log and subscription_log

--- app/models/membership_log.rb
+++ app/models/membership_log.rb
@@ -1,4 +1,10 @@
 class MembershipLog < ApplicationRecord
   belongs_to :user
   has_many :subscription_logs, dependent: :destroy
+
+  def apply_change
+    raise 'Job is already finished or canceled.' if self.finished? || self.canceled?
+    Membership.upsert(id: self.user_id, plan: self.plan, status: self.status)
+    self.update!(finished: true)
+  end
 end
--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,4 +1,30 @@
 class Subscription < ApplicationRecord
   belongs_to :user
   belongs_to :channel
+
+  delegate :email, prefix: true, to: :user
+  delegate :google_group_key, to: :channel
+
+  @@service = GoogleDirectoryService.instance
+
+  # GoogleGroupにメンバー追加
+  after_create do
+    return if google_group_key.blank?
+    member = Google::Apis::AdminDirectoryV1::Member.new(email: user_email)
+    @@service.insert_member(google_group_key, member)
+  end
+
+  # GoogleGroupで配信設定を変更（一時停止・再開）
+  after_update do
+    return if google_group_key.blank?
+    delivery_settings = self.paused ? 'NONE' : 'ALL_MAIL'
+    member = Google::Apis::AdminDirectoryV1::Member.new(delivery_settings: delivery_settings)
+    @@service.patch_member(google_group_key, user_email, member)
+  end
+
+  # GoogleGroupからメンバー削除
+  after_destroy do
+    return if google_group_key.blank?
+    @@service.delete_member(google_group_key, user_email)
+  end
 end
