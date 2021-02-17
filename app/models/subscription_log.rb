628a6c9753d413caaba514e4d03b7c87b00f2877
notsobad <tomomichi.onishi@gmail.com>
2021-02-17 16:38:06 +0900

    add apply_change method to membership_log and subscription_log

--- app/models/subscription_log.rb
+++ app/models/subscription_log.rb
@@ -2,4 +2,23 @@ class SubscriptionLog < ApplicationRecord
   belongs_to :user
   belongs_to :channel
   belongs_to :membership_log, required: false
+
+  def apply_change
+    raise 'Job is already finished or canceled.' if self.finished? || self.canceled?
+
+    sub = Subscription.find_by(user_id: self.user_id, channel_id: self.channel_id)
+    case self.action
+    when 'subscribe'
+      Subscription.create!(user_id: self.user_id, channel_id: self.channel_id)
+    when 'unsubscribe'
+      sub.destroy!
+    when 'pause'
+      sub.update!(paused: true)
+    when 'restart'
+      sub.update!(paused: false)
+    else
+      raise 'Required action is not defined.'
+    end
+    self.update!(finished: true)
+  end
 end
