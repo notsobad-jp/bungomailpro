d604487121a2f258b7588848e809befb14614371
notsobad <tomomichi.onishi@gmail.com>
2021-02-17 15:32:32 +0900

    add subscription and subscription_log

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ApplicationRecord
   has_one :membership, foreign_key: :id, dependent: :destroy
   has_many :membership_logs, dependent: :destroy
   has_many :channels, dependent: :destroy
+  has_many :subscriptions, dependent: :destroy
+  has_many :subscription_logs, dependent: :destroy
 
   delegate :stripe_customer_id, :stripe_subscription_id, :plan, :status, to: :membership, allow_nil: true
 
@@ -14,4 +16,22 @@ class User < ApplicationRecord
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
   validates :email, presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX }
+
+  def schedule_trial
+    ch_basic = Channel.find_by(code: 'bungomail-official')
+    ch_free = Channel.find_by(code: 'dogramagra') # FIXME: 無料プラン用チャネル作ってそっちに差し替える
+    start_at = Time.current.next_month.beginning_of_month
+    end_at = start_at.end_of_month
+
+    ActiveRecord::Base.transaction do
+      # トライアル開始時
+      m_log_start = self.membership_logs.create!(action: 'trial_start', plan: 'basic', status: "trialing", apply_at: start_at)
+      self.subscription_logs.create!(channel_id: ch_basic.id, action: 'subscribe', apply_at: start_at, membership_log_id: m_log_start.id)
+
+      # トライアル終了時
+      m_log_cancel = self.membership_logs.create!(action: 'cancel', plan: 'basic', status: "canceled", apply_at: end_at)
+      self.subscription_logs.create!(channel_id: ch_basic.id, action: 'unsubscribe', apply_at: end_at, membership_log_id: m_log_cancel.id)
+      self.subscription_logs.create!(channel_id: ch_free.id, action: 'subscribe', apply_at: start_at.next_month, membership_log_id: m_log_cancel.id)
+    end
+  end
 end
