61886b684d61e47e9188df42def4dd2aa898564c
notsobad <tomomichi.onishi@gmail.com>
2021-02-20 12:14:30 +0900

    add pausing subscription

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -1,4 +1,7 @@
 class SubscriptionsController < ApplicationController
+  # Subscriptionは直接更新せず、すべてSubscriptionLog経由でapply_allする（なので反映は即時じゃなくて翌日00:00）
+
+  # 購読
   def create
     @channel = Channel.find(params[:channel_id])
     begin
@@ -12,6 +15,22 @@ class SubscriptionsController < ApplicationController
     redirect_to channel_path(@channel)
   end
 
+  # 一時停止
+  def update
+    @sub = Subscription.find(params[:id])
+    begin
+      google_action = 'update' if @sub.channel.google_group_key.present?
+      current_user.subscription_logs.create!(channel_id: @sub.channel_id, status: 'paused', google_action: google_action)
+      current_user.subscription_logs.create!(channel_id: @sub.channel_id, status: 'active', apply_at: beginning_of_next_month, google_action: google_action)
+      flash[:success] = '配信を月末まで一時停止しました。翌月から自動的に配信を再開します。'
+    rescue => e
+      logger.error "[Error]Pausing failed: #{e.message}, #{current_user.id}"
+      flash[:error] = '【エラー】処理に失敗しました。。何回か試してもうまくいかない場合、お手数ですが運営までお問い合わせください。'
+    end
+    redirect_to channel_path(@sub.channel)
+  end
+
+  # 解約
   def destroy
     @sub = Subscription.find(params[:id])
     begin
