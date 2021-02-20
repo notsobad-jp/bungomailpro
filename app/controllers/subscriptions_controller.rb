e4eff52ae8a3d88a77a02cbf2863ab570c3cce20
notsobad <tomomichi.onishi@gmail.com>
2021-02-20 12:00:06 +0900

    refactor sub/unsub using sub_log

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -2,10 +2,11 @@ class SubscriptionsController < ApplicationController
   def create
     @channel = Channel.find(params[:channel_id])
     begin
-      @channel.add_subscriber(current_user)
-      flash[:success] = '登録完了しました！明日の配信からメールが届きます。<a href="https://blog.bungomail.com/entry/2018/05/21/172542" target="_blank" class="text-link">メールの受信設定</a>を事前に必ずご確認ください。'
+      google_action = 'insert' if @channel.google_group_key.present?
+      current_user.subscription_logs.create!(channel_id: @channel.id, status: 'active', google_action: google_action)
+      flash[:success] = '登録完了しました！翌日の配信からメールが届きます。<a href="https://blog.bungomail.com/entry/2018/05/21/172542" target="_blank" class="text-link">メールの受信設定</a>を事前に必ずご確認ください。'
     rescue => e
-      logger.error "[Subscription Error]#{e.message}, #{current_user.id}"
+      logger.error "[Error]Subscription failed: #{e.message}, #{current_user.id}"
       flash[:error] = '【エラー】処理に失敗しました。。何回か試してもうまくいかない場合、お手数ですが運営までお問い合わせください。'
     end
     redirect_to channel_path(@channel)
@@ -14,10 +15,11 @@ class SubscriptionsController < ApplicationController
   def destroy
     @sub = Subscription.find(params[:id])
     begin
-      @sub.channel.delete_subscriber(current_user)
+      google_action = 'delete' if @sub.channel.google_group_key.present?
+      current_user.subscription_logs.create!(channel_id: @sub.channel_id, status: 'canceled', google_action: google_action)
       flash[:success] = '購読解除しました！次回配信からメールが届かなくなります。'
     rescue => e
-      logger.error "[Unsubscription Error]#{e.message}, #{current_user.id}"
+      logger.error "[Error]Unsubscription failed: #{e.message}, #{current_user.id}"
       flash[:error] = '【エラー】処理に失敗しました。。何回か試してもうまくいかない場合、お手数ですが運営までお問い合わせください。'
     end
     redirect_to channel_path(@sub.channel)
--- app/models/channel.rb
+++ app/models/channel.rb
@@ -7,27 +7,4 @@ class Channel < ApplicationRecord
   has_many :active_subscribers, through: :active_subscriptions, source: :user
 
   delegate :title, :description, :google_group_key, to: :channel_profile, allow_nil: true
-
-
-  # チャネルの即時購読
-  def add_subscriber(user)
-    # subscription作成・更新
-    sub = Subscription.find_or_initialize_by(user_id: user.id, channel_id: self.id)
-    sub.update!(status: "active")
-
-    # 履歴管理のためにsubscription_logも作成
-    google_action = 'insert' if self.google_group_key.present?
-    user.subscription_logs.create!(channel_id: self.id, status: 'active', finished: true, google_action: google_action)
-  end
-
-  # チャネルの即時解約
-  def delete_subscriber(user)
-    # subscriptionの更新
-    sub = Subscription.find_by(user_id: user.id, channel_id: self.id)
-    sub.update!(status: "canceled")
-
-    # 履歴管理のためにsubscription_logも作成
-    google_action = 'delete' if self.google_group_key.present?
-    user.subscription_logs.create!(channel_id: self.id, status: 'canceled', finished: true, google_action: google_action)
-  end
 end
