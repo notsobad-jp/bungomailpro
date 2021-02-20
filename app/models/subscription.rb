d647725a21960f0335edb6977adec68f74b634f9
notsobad <tomomichi.onishi@gmail.com>
2021-02-20 14:48:09 +0900

    add google management for cancel_forbidden_subs

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -11,14 +11,20 @@ class Subscription < ApplicationRecord
     return if forbidden_subs.blank?
 
     ActiveRecord::Base.transaction(joinable: false, requires_new: true) do
+      # subscription停止
       forbidden_subs.update_all(status: 'canceled')
+
+      # 一応subscription_logも残す
       sub_logs = []
       forbidden_subs.each do |sub|
         google_action = 'delete' if sub.channel.google_group_key
-        sub_logs << sub.slice(:user_id, :channel_id, :status).merge(finished: true, google_action: google_action)
-        if google_action
-          # TODO: GoogleGroupからも削除
-        end
+        sub_log_attributes = sub.slice(:user_id, :channel_id, :status).merge(finished: true, google_action: google_action)
+        sub_logs << sub_log_attributes
+        next unless google_action
+
+        # GoogleGroupに紐づく場合はそっちの状態も更新
+        sub_log = SubscriptionLog.new(sub_log_attributes)
+        sub_log.update_google_subscription  # メソッド側で例外処理してるのでそのままでOK
       end
       SubscriptionLog.insert_all(sub_logs) if sub_logs.present?
     end
