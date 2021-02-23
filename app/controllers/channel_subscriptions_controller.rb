9b7a11c93068a3a554bd34f897dc5961e7b5f9f0
notsobad <tomomichi.onishi@gmail.com>
2021-02-23 16:39:43 +0900

    fix tmp sub/unsub to sync subscriptions

--- app/controllers/channel_subscriptions_controller.rb
+++ app/controllers/channel_subscriptions_controller.rb
@@ -1,55 +1,49 @@
 class ChannelSubscriptionsController < ApplicationController
   skip_before_action :require_login
 
+  @@official_channel_id = Channel.find_by(code: 'bungomail-official').id
+
   # 購読
   def create
-    service = GoogleDirectoryService.instance
-
     begin
-      member = Google::Apis::AdminDirectoryV1::Member.new(email: params[:email])
-      service.insert_member(ENV['GOOGLE_GROUP_KEY'], member)
-      flash[:success] = '登録完了しました！明日の配信からメールが届きます。<a href="https://blog.bungomail.com/entry/2018/05/21/172542" target="_blank" class="text-link">メールの受信設定</a>を事前に必ずご確認ください。'
-      ChannelSubscriptionLog.create!(email: params[:email], action: "subscribed")
+      user = User.find_or_create_by(email: params[:email])
+      if user.subscriptions.present?
+        flash[:error] = 'このメールアドレスはすでに登録されています'
+      else
+        user.subscription_logs.create(channel_id: @@official_channel_id, apply_at: Time.current, status: 'active', google_action: 'insert')
+        flash[:success] = '登録完了しました！翌日の配信からメールが届きます。<a href="https://blog.bungomail.com/entry/2018/05/21/172542" target="_blank" class="text-link">メールの受信設定</a>を事前に必ずご確認ください。'
+      end
     rescue => e
       logger.error "[Subscription Error]#{e.message}, #{params[:email]}"
-      flash[:error] = case e.status_code
-                      when 409
-                        'このメールアドレスはすでに登録されています'
-                      when 404
-                        'メールアドレスの登録に失敗しました。。再度試してもうまくいかない場合、お手数ですがinfo@notsobad.jpまでお問い合わせください。'
-                      else
-                        e.message
-                      end
+      flash[:error] = 'メールアドレスの登録に失敗しました。。再度試してもうまくいかない場合、お手数ですがinfo@notsobad.jpまでお問い合わせください。'
     end
     redirect_to root_path
   end
 
   # 配信停止
   def destroy
-    service = GoogleDirectoryService.instance
-
     begin
+      user = User.find_by(email: params[:email])
+      if !user || user.subscriptions.blank?
+        flash[:error] = 'メールアドレスが見つかりませんでした。。再度試してもうまくいかないない場合、お手数ですがinfo@notsobad.jpまでお問い合わせください。'
+        return redirect_to page_path(:unsubscribe)
+      end
+
       # 登録解除
       if params[:action_type] == 'unsubscribed'
-        service.delete_member(ENV['GOOGLE_GROUP_KEY'], params[:email])
+        user.subscription_logs.create(channel_id: @@official_channel_id, apply_at: Time.current, status: 'canceled', google_action: 'delete')
         flash[:success] = '購読を解除しました。明日の配信からメールが届かなくなります。これまでのご利用ありがとうございました。'
       # 月末まで一時停止
       elsif params[:action_type] == 'paused'
-        member = Google::Apis::AdminDirectoryV1::Member.new(delivery_settings: 'NONE')
-        service.patch_member(ENV['GOOGLE_GROUP_KEY'], params[:email], member)
+        user.subscription_logs.create(channel_id: @@official_channel_id, apply_at: Time.current, status: 'paused', google_action: 'update')
+        user.subscription_logs.create(channel_id: @@official_channel_id, apply_at: Time.current.next_month.beginning_of_month, status: 'active', google_action: 'update')
         flash[:success] = '月末まで配信を一時停止しました。来月になると自動で配信が再開されます。'
       end
 
-      ChannelSubscriptionLog.create!(email: params[:email], action: params[:action_type])
       redirect_to root_path
     rescue => e
       logger.error "[Unsubscription Error]#{e.message}, #{params[:email]}"
-      flash[:error] = case e.status_code
-                      when 404
-                        'メールアドレスが見つかりませんでした。。再度試してもうまくいかないない場合、お手数ですがinfo@notsobad.jpまでお問い合わせください。'
-                      else
-                        e.message
-                      end
+      flash[:error] = '処理に失敗しました。。再度試してもうまくいかないない場合、お手数ですがinfo@notsobad.jpまでお問い合わせください。'
       redirect_to page_path(:unsubscribe)
     end
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -15,9 +15,7 @@ class User < ApplicationRecord
   end
 
   after_create do
-    # 正式リリース前に登録したら2021年6月末までトライアル。それ以降は翌月末までトライアル。
-    trial_end_at = [Time.zone.parse("2021/06/01").end_of_month, Time.current.next_month.end_of_month].max
-    Membership.create!(id: self.id, plan: 'free', status: 'active', trial_end_at: trial_end_at)
+    Membership.create!(id: self.id, plan: 'free', status: 'active')
   end
 
   validates :email, presence: true, uniqueness: true
