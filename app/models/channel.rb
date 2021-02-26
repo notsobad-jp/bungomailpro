07488509dd67d4360c511b1d86abbd360e54d737
notsobad <tomomichi.onishi@gmail.com>
2021-02-26 21:11:29 +0900

    wip: update job's hour on channel update

--- app/models/channel.rb
+++ app/models/channel.rb
@@ -9,6 +9,13 @@ class Channel < ApplicationRecord
 
   delegate :title, :description, :google_group_key, to: :channel_profile, allow_nil: true
 
+  # delivery_timeが更新されたときは予約中のjobの配信時間も更新する
+  after_update  :update_jobs_delivery_time, if: :saved_change_to_delivery_time?
+
   OFFICIAL_CHANNEL_ID = '1418479c-d5a7-4d29-a174-c5133ca484b6'
   JUVENILE_CHANNEL_ID = '470a73fb-d1ae-4ffb-9c6b-5b9dc292f4ef'
+
+  
+  def update_jobs_delivery_time
+  end
 end
