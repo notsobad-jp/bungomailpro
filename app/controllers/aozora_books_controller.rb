c60885b051eafd760d8e19cd533289aef7fa29b7
notsobad <tomomichi.onishi@gmail.com>
2021-03-09 22:57:29 +0900

    add back to search

--- app/controllers/aozora_books_controller.rb
+++ app/controllers/aozora_books_controller.rb
@@ -2,14 +2,15 @@ class AozoraBooksController < ApplicationController
   skip_before_action :require_login, only: [:index, :show]
 
   def index
-    @meta_title = '青空文庫の作品検索'
     @q = search_params
     @books = AozoraBook.search(@q).sorted.page(params[:page]).per 10
+    @meta_title = '青空文庫の作品検索'
   end
 
   def show
     @book = AozoraBook.find(params[:id])
     @book_assignment = BookAssignment.new
+    @q = search_params
     @meta_title = @book.title
     @breadcrumbs = [
       {text: '作品検索', link: aozora_books_path},
