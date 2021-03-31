a935d691b18bccb0e7e8bfa1e97f7c58ea736813
notsobad <tomomichi.onishi@gmail.com>
2021-03-31 14:11:13 +0900

    tmp fix for subscriptoin restart

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -16,7 +16,17 @@ class Subscription < ApplicationRecord
   def self.restart_all
     self.includes(channel: :channel_profile).where(paused: true).each do |sub|
       sub.update!(paused: false)
-      sleep 0.5 if sub.channel.google_group_key.present?
+
+      # 2021.3.31: まだcallback設定してないのでGroupの配信設定を手動変更
+      # sleep 0.5 if sub.channel.google_group_key.present?
+      begin
+        member = Google::Apis::AdminDirectoryV1::Member.new(delivery_settings: 'ALL_MAIL')
+        @@service.update_member('bungomail-text@notsobad.jp', sub.user_email, member)
+        sleep 1
+      rescue => e
+        logger.error "[Error] google update failed: #{sub.id} #{e}"
+        e
+      end
     end
   end
 
