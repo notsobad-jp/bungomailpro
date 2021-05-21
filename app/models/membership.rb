9df1d4f78a74f77ba2c73f65216a1622bcecfa26
notsobad <tomomichi.onishi@gmail.com>
2021-05-21 12:09:29 +0900

    fix membership logic

--- app/models/membership.rb
+++ app/models/membership.rb
@@ -2,16 +2,12 @@ class Membership < ApplicationRecord
   belongs_to :user, foreign_key: :id
   has_many :membership_logs, foreign_key: :user_id
 
-  MAXIMUM_SUBSCRIPTIONS_COUNT = { free: 1, basic: 5 }.freeze
+  MAXIMUM_SUBSCRIPTIONS_COUNT = { free: 1, basic: 3 }.freeze
 
-  # freeプラン開始（activate時）: 児童チャネル購読
-  after_create do
-    user.subscriptions.new(channel_id: Channel::JUVENILE_CHANNEL_ID).save!(validate: false)
-  end
-
-  # trial開始: 公式チャネル購読
-  after_update if: :trialing_started? do
-    user.email_digest.update!(trial_ended_at: trial_end_at)
+  # basicプラン開始: 公式チャネル購読
+  ## 基本はbasicプランのトライアル開始時。再課金のケースではトライアルなしでいきなり開始パターンもありえる
+  after_update if: :basic_plan_started? do
+    user.email_digest.update!(trial_ended_at: self.trial_end_at)
     user.subscriptions.new(channel_id: Channel::OFFICIAL_CHANNEL_ID).save!(validate: false) # FIXME: subscriptionsのvalidationが先に来てしまうので一旦スキップ
   end
 
@@ -21,29 +17,16 @@ class Membership < ApplicationRecord
     user.subscriptions.where.not(channel_id: Channel::JUVENILE_CHANNEL_ID).destroy_all # 児童チャネル以外配信停止
   end
 
-  # 退会: すべてのチャネル解約・自作チャネルの削除・配信停止
-  after_update if: :free_plan_canceled? do
-    user.destroy!
-  end
 
-
-  def schedule_trial(apply_at)
-    raise 'trial ended' if plan == 'basic' || trial_end_at.present?
-    self.membership_logs.create!(plan: 'basic', trialing: true, apply_at: apply_at)
-    self.membership_logs.create!(plan: 'free', apply_at: apply_at.since(1.month)) # 変更がなければ1ヶ月後にfreeプランに戻る
-    self.update!(trial_end_at: apply_at.end_of_month)
-  end
-
-  # 決済情報登録して、翌月末かトライアル終了後から課金開始
+  # 翌月初からトライアル開始
   def schedule_billing(apply_at)
-    raise 'already billing' if plan == 'basic' && !trialing?   # ありえるのは free-active OR basic-trialingからの登録
-    self.membership_logs.scheduled.update_all(canceled: true)
+    raise 'already billing' if plan != 'free'
     self.membership_logs.create!(plan: 'basic', apply_at: apply_at)
   end
 
   # 月末で解約してfreeプランに戻る
   def schedule_cancel(apply_at)
-    raise 'not billing' if plan == 'free' || trialing?
+    raise 'not billing' if plan == 'free'
     self.membership_logs.create!(plan: 'free', apply_at: apply_at)
   end
 
@@ -59,19 +42,11 @@ class Membership < ApplicationRecord
   ###########################################################
   # callbackの条件判定メソッド
   ###########################################################
-  def plan_or_trialing_changed?
-    (saved_changes.keys & ['plan', 'trialing']).present?
-  end
-
-  def trialing_started?
-    plan_or_trialing_changed? && plan == 'basic' && trialing?
+  def basic_plan_started?
+    saved_change_to_plan == ['free', 'basic']
   end
 
   def basic_plan_canceled?
     saved_change_to_plan == ['basic', 'free']
-  end
-
-  def free_plan_canceled?
-    plan_or_trialing_changed? && plan == nil
   end
 end
--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -6,13 +6,6 @@ class Subscription < ApplicationRecord
   validate :check_subscriptions_count, on: :create
   delegate :email, prefix: true, to: :user
 
-  @@service = GoogleDirectoryService.instance
-
-  # FIXME: データ移行方針の整理が終わるまでは事故防止のため一時停止
-  # after_create  :google_insert_member, if: Proc.new { |sub| sub.channel.google_group_key.present? }
-  # after_update  :google_update_member, if: Proc.new { |sub| sub.channel.google_group_key.present? }
-  # after_destroy :google_delete_member, if: Proc.new { |sub| sub.channel.google_group_key.present? }
-
   def self.restart_all
     self.includes(channel: :channel_profile).where(paused: true).each do |sub|
       sub.update!(paused: false)
@@ -42,35 +35,5 @@ class Subscription < ApplicationRecord
   def check_subscriptions_count
     return if user.subscriptions.count < Membership::MAXIMUM_SUBSCRIPTIONS_COUNT[user.plan.to_sym]
     errors.add(:base, "購読上限数を超えています。他のチャネルの購読を解除するか、プランをアップグレードしてください。")
-  end
-
-  def google_insert_member
-    begin
-      member = Google::Apis::AdminDirectoryV1::Member.new(email: user_email)
-      @@service.insert_member(channel.google_group_key, member)
-    rescue => e
-      logger.error "[Error] google insert failed: #{id} #{e}"
-      e
-    end
-  end
-
-  def google_update_member
-    begin
-      delivery_settings = paused? ? 'NONE' : 'ALL_MAIL'
-      member = Google::Apis::AdminDirectoryV1::Member.new(delivery_settings: delivery_settings)
-      @@service.update_member(channel.google_group_key, user_email, member)
-    rescue => e
-      logger.error "[Error] google update failed: #{id} #{e}"
-      e
-    end
-  end
-
-  def google_delete_member
-    begin
-      @@service.delete_member(channel.google_group_key, user_email)
-    rescue => e
-      logger.error "[Error] google delete failed: #{id} #{e}"
-      e
-    end
   end
 end
