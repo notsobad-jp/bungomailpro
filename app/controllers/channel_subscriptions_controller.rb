8df97ef3dd93b87564266ed75cbf97c580187c27
notsobad <tomomichi.onishi@gmail.com>
2021-07-09 17:26:56 +0900

    skip subscription management

--- app/controllers/channel_subscriptions_controller.rb
+++ app/controllers/channel_subscriptions_controller.rb
@@ -7,17 +7,9 @@ class ChannelSubscriptionsController < ApplicationController
     service = GoogleDirectoryService.instance
     user = User.find_or_create_by(email: params[:email])
 
-    # すでに購読済みの場合はエラー
-    if user.subscriptions.present?
-      return redirect_to root_path, flash: { error: 'このメールアドレスはすでに登録されています' }
-    elsif user.errors.present?
-      return redirect_to root_path, flash: { error: '登録に失敗しました。。Gmailのエイリアスアドレスなどは利用できませんので、他のアドレスをご利用ください。' }
-    end
-
     begin
       member = Google::Apis::AdminDirectoryV1::Member.new(email: params[:email])
       service.insert_member(ENV['GOOGLE_GROUP_KEY'], member)
-      user.subscriptions.create(channel_id: Channel::OFFICIAL_CHANNEL_ID)
       flash[:success] = '登録完了しました！翌日の配信からメールが届きます。<a href="https://blog.bungomail.com/entry/2018/05/21/172542" target="_blank" class="text-link">メールの受信設定</a>を事前に必ずご確認ください。'
     rescue => e
       logger.error "[Subscription Error]#{e.message}, #{params[:email]}"
@@ -30,11 +22,6 @@ class ChannelSubscriptionsController < ApplicationController
   def destroy
     service = GoogleDirectoryService.instance
     user = User.find_by(email: params[:email])
-    sub = user.subscriptions.find_by(channel_id: Channel::OFFICIAL_CHANNEL_ID) if user
-    if !user || !sub
-      flash[:error] = 'メールアドレスが見つかりませんでした。。再度試してもうまくいかないない場合、お手数ですがinfo@notsobad.jpまでお問い合わせください。'
-      return redirect_to page_path(:unsubscribe)
-    end
 
     # 登録解除
     begin
@@ -46,7 +33,6 @@ class ChannelSubscriptionsController < ApplicationController
       elsif params[:action_type] == 'paused'
         member = Google::Apis::AdminDirectoryV1::Member.new(delivery_settings: 'NONE')
         service.patch_member(ENV['GOOGLE_GROUP_KEY'], params[:email], member)
-        sub.update!(paused: true)
         flash[:info] = '月末まで配信を一時停止しました。来月になると自動で配信が再開されます。'
       end
       redirect_to root_path
